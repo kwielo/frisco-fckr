8bac807b09d8c020035330c03201c4daabc907cb
Krzysztof <kwielogorski@gmail.com>
2020-04-06 12:46:36 +0200

    update

--- reservation.js
+++ reservation.js
@@ -1,9 +1,11 @@
 function app() {
 
   return {
-    checkAgainAfter: 20000, // miliseconds
+    checkAgainAfter: 500, // miliseconds
     deliveryDate: '2020/5/25',
     userId: '527522',
+    division: 'WAW',
+    features: 'BalanceAmount=1',
     visitorId: "f269f08c-1fe4-4eac-83fa-ea12ee9cc1cf",
     token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1Mjc1MjIiLCJ2aXNpdG9yX2lkIjoiZjI2OWYwOGMtMWZlNC00ZWFjLTgzZmEtZWExMmVlOWNjMWNmIiwidG9rZW5fdXNhZ2UiOiJhY2Nlc3NfdG9rZW4iLCJqdGkiOiI5MGY1ODFlZS05NDMwLTQwNmMtYjdkMi1lYjliMTQ3YTNmZjkiLCJzY29wZSI6Im9mZmxpbmVfYWNjZXNzIiwibmJmIjoxNTg2MTY4MDY0LCJleHAiOjE1ODYxNjg2NjQsImlhdCI6MTU4NjE2ODA2NCwiaXNzIjoiaHR0cHM6Ly9jb21tZXJjZS5mcmlzY28ucGwvIn0.sKXCdEnevjL40B-vzh90OV6-F918hWU_Mou4z5K5a5Q', // 600 seconds timeout
     getRefreshTokenUrl() {
@@ -26,8 +28,8 @@ function app() {
         h.open("GET", this.getSlotsUrl(), false);
 
         h.setRequestHeader("Authorization", this.getAuth());
-        h.setRequestHeader("x-frisco-division", "WAW");
-        h.setRequestHeader("x-frisco-features", "BalanceAmount=1");
+        h.setRequestHeader("x-frisco-division", this.division);
+        h.setRequestHeader("x-frisco-features", this.features);
         h.setRequestHeader("x-frisco-visitorid", this.visitorId);
         
         try {
@@ -47,8 +49,8 @@ function app() {
 
       h.setRequestHeader("Content-Type", "application/json");
       h.setRequestHeader("Authorization", this.getAuth());
-      h.setRequestHeader("x-frisco-division", "WAW");
-      h.setRequestHeader("x-frisco-features", "BalanceAmount=1");
+      h.setRequestHeader("x-frisco-division", this.division);
+      h.setRequestHeader("x-frisco-features", this.features);
       h.setRequestHeader("x-frisco-visitorid", this.visitorId);
       
       try {
